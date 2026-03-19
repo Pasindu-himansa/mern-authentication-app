1db5f061b9b66ae961d76d9ab892d5e6c45bb060
Pasindu Subasinghe <phimansa@gmail.com>
2026-03-19 23:05:14 +0530

    DNS issue fixed server.js updated

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,18 @@
+import dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.js";
 import { connectDB } from "./config/db.js";
+
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-
 const app = express();
 
 app.use(express.json());
-
 app.use("/api/users", authRoutes);
 
 connectDB();
